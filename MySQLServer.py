d638c15c200ed7eed4f045e714f3e6ec86c57eab
Foreversengine <opeloyeruwasiu123@gmail.com>
2025-02-02 11:30:58 +0000

    Let's Build Your Database: Your Gateway to Data Adventure!

---
+++ MySQLServer.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="iviidev"
+    )
+    
+    if connection.is_connected():
+        my_cursor = connection.cursor()
+        my_cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+        print("Database 'alx_book_store' created successfully!")
+    
+except mysql.connector.Error as err:
+    print(f"Error while creating database {err}")
+
+finally:
+    if connection.is_connected():
+        connection.close()
+        my_cursor.close()
+        print("MYSQL connection is closed.")
